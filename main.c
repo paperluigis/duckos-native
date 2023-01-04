a77988b7b97de5bb341122f09329859ec3752c00
paperluigis <a@b.co>
2023-01-04 18:42:00 +0500

    go back to inline brackets

--- main.c
+++ main.c
@@ -10,8 +10,7 @@ SDL_Renderer *renderer;
 TTF_Font *font_monospace;
 TTF_Font *font_sans;
 
-void handle_exit()
-{
+void handle_exit() {
     puts("ur sus");
 
     // SDL_FreeSurface(surface);
@@ -26,14 +25,11 @@ void handle_exit()
     exit(0);
 }
 
-int poll()
-{
+int poll() {
     SDL_Event next_event;
 
-    if (SDL_PollEvent(&next_event))
-    {
-        switch (next_event.type)
-        {
+    if (SDL_PollEvent(&next_event)) {
+        switch (next_event.type) {
         case SDL_QUIT:
             handle_exit();
             break;
@@ -41,22 +37,23 @@ int poll()
     }
 }
 
-int draw()
-{
+int draw() {
     SDL_RenderClear(renderer);
     SDL_RenderPresent(renderer);
-    SDL_SetRenderDrawColor(renderer, DUCKOS_ACCENT_BACKGROUND);
+    SDL_SetRenderDrawColor(renderer,
+        duckos_configuration.color.background.r,
+        duckos_configuration.color.background.g,
+        duckos_configuration.color.background.b,
+        duckos_configuration.color.background.a);
     // SDL_UpdateWindowSurface(window);
 }
 
-int main(int argc, char *const argv[])
-{
+int main(int argc, char *const argv[]) {
     // initialize config struct
     load_config();
 
     // initialize sdl
-    if (SDL_Init(SDL_INIT_VIDEO) < 0)
-    {
+    if (SDL_Init(SDL_INIT_VIDEO) < 0) {
         fputs("SDL start vented\n", stderr);
         return 1;
     }
@@ -67,23 +64,20 @@ int main(int argc, char *const argv[])
     // load in some fonts
     font_monospace = TTF_OpenFont(DUCKOS_FONT_LOCATION_MONO, DUCKOS_FONT_SIZE_TERM);
     font_sans = TTF_OpenFont(DUCKOS_FONT_LOCATION_SANS, DUCKOS_FONT_SIZE_WINTITLE);
-    if (!font_sans)
-    {
+    if (!font_sans) {
         fputs("failed to load " DUCKOS_FONT_LOCATION_SANS, stderr);
         return 1;
     }
-    if (!font_monospace)
-    {
+    if (!font_monospace) {
         fputs("failed to load " DUCKOS_FONT_LOCATION_MONO, stderr);
         return 1;
     }
 
     // create window
     window = SDL_CreateWindow("DuckOS Native",
-                              SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 960, 540,
-                              SDL_WINDOW_OPENGL);
-    if (!window)
-    {
+        SDL_WINDOWPOS_CENTERED, SDL_WINDOWPOS_CENTERED, 960, 540,
+        SDL_WINDOW_OPENGL);
+    if (!window) {
         fputs("Window creation vented\n", stderr);
         return 1;
     }
@@ -91,14 +85,12 @@ int main(int argc, char *const argv[])
     // get window renderer
     // surface = SDL_GetWindowSurface(window);
     renderer = SDL_CreateRenderer(window, -1, SDL_RENDERER_ACCELERATED);
-    if (!renderer)
-    {
+    if (!renderer) {
         fputs("Renderer getting vented\n", stderr);
         return 1;
     }
 
-    while (1)
-    {
+    while (1) {
         poll();
         draw();
     }
